e362ebfaf8aaffdb32626b6514c1bb22c7c66389
Grant Bradley <grantbradleytiger13@gmail.com>
2022-11-24 08:14:15 -0500

    Initial Commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var grantsAccount = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            grantsAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is ${grantsAccount.GetBalance()}");
         }
     }
 }
